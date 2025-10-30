b48a5a4811d083a1c0661bf81d654eecd7844259
negi <nezii0331@gmail.com>
2025-10-30 20:33:35 +0800

    finished the rename words

--- crawler/parse.py
+++ crawler/parse.py
@@ -2,6 +2,7 @@
 # to transform datas (e.g. data cleaning, data normalize, data validated)
 # Cleaned DataFrame (pandas + regex)
 
+from ossaudiodev import control_names
 import pandas as pd; #Python library used for working with tabular data (rows and columns), similar to Excel
 import re;  #regex(search, match, and clean)
 from typing import Dict, List, Optional;
@@ -34,9 +35,24 @@ class HKJCparser:
             "Rank", "Horse_name","Age","Sex", "Trainer", "Rating", "Country","Surface", "Race_name"
         ]
 
-if __name__ == "__main__":
-    parser = HKJCparser();
+    # rename words
+    def _rename_columns(self, df: pd.DataFrame) ->pd.DataFrame:
+        new_columns = []  #store new columns's name
+        for col in df.columns:
+            col_clean = col.strip().lower() #remove blank and turn into small namme
+            if col_clean in self.alias:
+                new_columns.append(self.alias[col_clean])
+            else:
+                new_columns.append(col) #otherwise stay original name
+        df.columns = new_columns
+        return df 
+
+# test
+if __name__ == "__main__": 
+    parser = HKJCparser() #call __init__
     print("\n--- Testing constructor ---");
     print("Alias mapping keys:", list(parser.alias.keys()));
     print("Required columns:", parser.required_columns);
-
+    df = pd.DataFrame(columns = ["Horse Name", "Ranking", "YOF", "coach"])
+    new_df = parser._rename_columns(df)
+    print("Test new columns:", (list(new_df.columns)))
